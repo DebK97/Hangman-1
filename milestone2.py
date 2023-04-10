c40ad0a235763ff5e3b0bd426dcb2cbab05c694a
Deborah <deborahakuku@gmail.com>
2023-04-10 21:13:21 +0100

    This creates the possible word choices for the Hangman game. This is also the firdt stage of user input by guessing the correct letter.

--- milestone2.py
+++ milestone2.py
@@ -1,4 +1,34 @@
 word_list = ['strawberry', 'apple', 'banana', 'orange', 'pear']
 print('word_list')
-
-
+x = 'word list'
+print(x.split())
+print(x.split('l'))
+print(x.split("D"))
+x = 'HELLO WORLD'
+print(x.split())
+print(x.split("o"))
+print(x.split("W"))
+print("The {} {} {}" .format("fox", "brown", "quick"))
+print("The {2} {1} {0}" .format("fox", "brown", "quick"))
+print("The {0} {2} {1}" .format("fox", "brown", "quick"))
+word_list = 'strawberry'
+print(word_list[6])
+print(word_list[:9])
+my_string = "Hello World"
+print(my_string)
+print(type(my_string))
+my_string = "Hello\nWorld"
+print(my_string)
+my_string = "Hello\tWorld"
+print(my_string)
+my_boolean = True
+print(my_boolean)
+a = 5
+b = 2
+c = a > b
+print(c)
+print(type(c))
+d = a < b
+print(d)
+print(type(d))
+print(a <= b or a > b)
--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,20 @@
-word_list = ['strawberry', 'apple', 'banana', 'orange', 'pear']
-print(word_list)
+import random
 
+print("Welcome to Hangman!")
+print("------------------------------------------")
+
+# Possible word choices 
+word_list = ['strawberry', 'apple', 'banana', 'orange', 'pear']
+word = random.choice(word_list)
+
+guess = input("Please enter a letter: ")
+
+# making sure "guess" equals to 1 and is alphabetical
+if len(guess) == 1 and guess.isalpha() :
+  print("Good Guess!")
+else :
+  print("Oops! That's not a valid input.")  
+
+
+
+print(word)
