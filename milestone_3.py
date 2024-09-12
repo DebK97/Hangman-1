5b776a3a8f926185d9fb63f5f03d9a052d746174
Deborah <deborahakuku@gmail.com>
2024-09-12 13:48:36 +0100

    Adds functions to guess if letter is in the word and checks if the input is valid.

--- milestone_3.py
+++ milestone_3.py
@@ -10,9 +10,29 @@ word = random.choice(word_list)
 
 guess = input("Please enter a letter: ")
 
-while len(guess) == 1 and guess.isalpha() :
-    break
-else :
-    print("Invalid letter. Please, enter a single alphabetical character.")
+def check_guess(guess) :
+  guess.lower()  
+  if guess in word :
+    print(f"Good guess! {guess} is in the word.")
+  else :
+    print(f"Sorry, {guess} is not in the word. Try again.")   
 
-    
+ 
+
+def ask_for_input() :
+    while len(guess) == 1 and guess.isalpha() :
+     break
+    else :
+     print("Invalid letter. Please, enter a single alphabetical character.")
+
+    check_guess(guess)  
+
+ask_for_input()    
+
+ 
+
+  
+
+pass
+
+
